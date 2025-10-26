7132705bd488cc3fe3f87c0c605eac12c1fb61ca
SirBlobman <blobmansrealm@gmail.com>
2025-10-26 13:59:59 -0400

    Switch to auto-generated plugin.yml due to configuration cache issues.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,41 +1,47 @@
-val baseVersion = fetchProperty("version.base", "invalid")
-val betaString = fetchProperty("version.beta", "false")
-val jenkinsBuildNumber = fetchEnv("BUILD_NUMBER", null, "Unofficial")
-
-val betaBoolean = betaString.toBoolean()
-val betaVersion = if (betaBoolean) "Beta-" else ""
-version = "$baseVersion.$betaVersion$jenkinsBuildNumber"
-
-fun fetchProperty(propertyName: String, defaultValue: String): String {
-    val found = findProperty(propertyName)
-    if (found != null) {
-        return found.toString()
-    }
-
-    return defaultValue
-}
-
-fun fetchEnv(envName: String, propertyName: String?, defaultValue: String): String {
-    val found = System.getenv(envName)
-    if (found != null) {
-        return found
-    }
-
-    if (propertyName != null) {
-        return fetchProperty(propertyName, defaultValue)
-    }
-
-    return defaultValue
-}
+val base = providers.gradleProperty("version.base").orElse("invalid").get()
+val betaBoolean = providers.gradleProperty("version.beta").orElse("false").get().toBoolean()
+val build = providers.environmentVariable("BUILD_NUMBER").orElse("Unofficial").get()
+val beta = if (betaBoolean) "Beta-" else ""
+version = "$base.$beta$build"
 
 plugins {
     id("java")
+    id("de.eldoria.plugin-yml.bukkit") version "0.8.0"
 }
 
 java {
     sourceCompatibility = JavaVersion.VERSION_21
     targetCompatibility = JavaVersion.VERSION_21
     toolchain.languageVersion.set(JavaLanguageVersion.of(21))
+}
+
+bukkit {
+    name = "CompressedHearts"
+    prefix = "Compressed Hearts"
+    description="A plugin that allows you to change the heart scale of your players"
+    website="https://www.spigotmc.org/resources/44024/"
+
+    main="com.github.sirblobman.compressed.hearts.HeartsPlugin"
+    apiVersion = "1.21.8"
+
+    foliaSupported = true
+    authors = listOf("SirBlobman")
+    depend = listOf("BlueSlimeCore")
+
+    commands {
+        register("compressed-hearts") {
+            description = "Main command for the CompressedHearts plugin"
+            usage = "/<command> help"
+            aliases = listOf("compressedhearts", "chearts", "compressedh", "ch")
+        }
+
+        register("hp") {
+            description = "Check you current health."
+            usage = "/<command> [player]"
+            aliases = listOf("hearts", "health")
+        }
+    }
+
 }
 
 repositories {
@@ -47,7 +53,7 @@ repositories {
 
 dependencies {
     compileOnly("org.jetbrains:annotations:26.0.2") // JetBrains Annotations
-    compileOnly("org.spigotmc:spigot-api:1.21.8-R0.1-SNAPSHOT") // Spigot API
+    compileOnly("org.spigotmc:spigot-api:1.21.10-R0.1-SNAPSHOT") // Spigot API
     compileOnly("com.github.sirblobman.api:core:2.9-SNAPSHOT") // BlueSlimeCore
 }
 
@@ -66,35 +72,5 @@ tasks {
         options.encoding = "UTF-8"
         val standardOptions = options as StandardJavadocDocletOptions
         standardOptions.addStringOption("Xdoclint:none", "-quiet")
-    }
-
-    processResources {
-        val pluginName = fetchProperty("bukkit.plugin.name", "")
-        val pluginPrefix = fetchProperty("bukkit.plugin.prefix", "")
-        val pluginDescription = fetchProperty("bukkit.plugin.description", "")
-        val pluginWebsite = fetchProperty("bukkit.plugin.website", "")
-        val pluginMainClass = fetchProperty("bukkit.plugin.main", "")
-
-        filesMatching("plugin.yml") {
-            expand(
-                mapOf(
-                    "pluginName" to pluginName,
-                    "pluginPrefix" to pluginPrefix,
-                    "pluginDescription" to pluginDescription,
-                    "pluginWebsite" to pluginWebsite,
-                    "pluginMainClass" to pluginMainClass,
-                    "pluginVersion" to version
-                )
-            )
-        }
-
-        filesMatching("config.yml") {
-            expand(
-                mapOf(
-                    "pluginPrefix" to pluginPrefix,
-                    "pluginVersion" to version
-                )
-            )
-        }
     }
 }
